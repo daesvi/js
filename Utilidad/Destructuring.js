12d8ada4ff7dce3bec7d18b3fb285d848256d768
daesvi <villegasdaniel4321@gmail.com>
2022-06-15 15:05:33 -0500

    Destructuring y Spread

---
+++ Utilidad/Destructuring.js
@@ -0,0 +1,3 @@
+const usuario = ['Samuel', 'Villegas', '3006184535', 'Calle 66 # 50 40 int 202'];
+const [nombre, apellido, numero] = usuario;
+console.log(nombre,numero,usuario[3]); //Samuel 3006184535 Calle 66 # 50 40 int 202
---
+++ Utilidad/Spread.js
@@ -0,0 +1,4 @@
+const vector = [1, 2, 3, 4, 5]
+console.log(...vector) // Para mostrar los valores de ese vector
+console.log(Math.max(...vector));
+console.log(Math.min(...vector));
