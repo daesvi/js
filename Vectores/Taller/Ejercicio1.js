bd6102ba0d2097b4489801ff5012bee05d6a2344
daesvi <villegasdaniel4321@gmail.com>
2022-06-16 16:05:46 -0500

    Ejercicios resueltos

---
+++ Vectores/Taller/Ejercicio1.js
@@ -0,0 +1,9 @@
+/* Crear un arreglo de 6 posiciones con los días de la semana:
+Lunes, Martes, Miércoles, Jueves, Viernes y Sábado. 
+Adicionar Domingo con push. */
+
+const dias = ['Lunes','Martes','Miercoles','Jueves','Viernes','Sábado'];
+
+dias.push('Domingo');
+
+console.log(...dias);
